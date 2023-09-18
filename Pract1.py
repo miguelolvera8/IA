6ca3ce059cde804ddd9dd5d403ea7040ee8bf8b5
miguelolvera8 <miguelolverar@yahoo.com>
2023-09-17 21:12:09 -0600

    Initial commit

---
+++ Pract1.py
@@ -0,0 +1,7 @@
+
+mensaje = "HOLA"#asignamos a mensaje el string
+v1 = 13         #asignamos el primer valor entero a v1
+v2 = 15         #asignamos el valor entero a v2
+res = v1+v2     #asignamos la suma de v1 y v2 a res
+print(res)      #imprimimos y mandamos como argumento el resultado de la variable res
+print(mensaje)
